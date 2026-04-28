9203ac8f6c4a10bf855a3b8b27a2e95d71e6a3a6
Nick Ashworth <jamesnashworth@gmail.com>
2026-04-27 23:45:44 -0400

    test

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_1sJ112 from "./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md";
 import __ASTRO_IMAGE_IMPORT_16lcX8 from "./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md";
-export default new Map([["./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_1sJ112], ["./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_16lcX8]]);
+import __ASTRO_IMAGE_IMPORT_1sJ112 from "./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md";
+export default new Map([["./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_16lcX8], ["./avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_1sJ112]]);
 		
